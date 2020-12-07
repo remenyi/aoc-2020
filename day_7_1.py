6525ae16d791f60bff87b45bf41bb0a44421a63f
gergo <remenyig@gmail.com>
2020-12-07 18:32:15 +0100

    fix filename mismatch + cleanup

--- day_7_1.py
+++ day_7_1.py
@@ -1,18 +1,25 @@
 import re
-import functools
-import operator
 
-def get_bag_no(ip, e):
-    values = ip[e]
-    if values[0][0] == "no other":
-        return 0
-    no = 0
-    for l in values:
-        no += get_bag_no(ip, l[0])*int(l[1])+int(l[1])
-    return no
+def get_keys(ip, e):
+    out = []
+    for k in ip:
+        if e in ip[k]:
+            out.append(k)
+    return out
+
+def get_containers(ip, e):
+    containers = get_keys(ip, e)
+    if containers == []:
+        return set()
+    containers = set(containers)
+    new_containers = set()
+    for l in containers:
+        new_containers.update(get_containers(ip, l))
+    containers.update(new_containers)
+    return containers
 
 with open("day_7_input.txt") as ip:
     rules = {re.sub("bags", "", i.split("contain")[0]).strip() : \
-        [[re.sub("[0-9]*|bag(s)*", "", j).strip(" ."), re.sub("[a-zA-Z]*", "", j).strip(" .")] for j in i.split("contain")[1].split(",")] \
+        [re.sub("[0-9]*|bag(s)*", "", j).strip(" .") for j in i.split("contain")[1].split(",")] \
         for i in ip.read().splitlines()}
-    print(get_bag_no(rules, "shiny gold"))
+    print(len(get_containers(rules, "shiny gold")))
